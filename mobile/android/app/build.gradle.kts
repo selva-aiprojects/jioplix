b183808c6495b16b025c7491411f689b4a9e933c
selva-aiprojects <selva@aiprojects.com>
2026-07-20 22:36:26 +0530

    Build lightweight Android split APKs and link in web client

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
